8d3669e628c5933ebfd27323944097b15f11f23d
Bandurski, Piotr <piotr.bandurski@intel.com>
2017-06-20 13:58:13 +0200

    put done

--- resources/car.py
+++ resources/car.py
@@ -1,7 +1,32 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 import sqlobject as sq
 
 from models.model import *
+
+
+def owner(owner_id):
+    # validate owner_id
+    try:
+        Owners.get(owner_id)
+    except sq.main.SQLObjectNotFound:
+        raise ValueError({"Owner id:{} doesn't exist"}.format(owner_id))
+    return owner_id
+
+
+put_parser = reqparse.RequestParser()
+put_parser.add_argument('brand', dest='brand',
+                         location='form',
+                         help='Car\'s brand')
+put_parser.add_argument('model', dest='model',
+                         location='form',
+                         help='Car\'s model')
+put_parser.add_argument('number', dest='number',
+                         location='form',
+                         help='Car\'s number')
+put_parser.add_argument('owner', dest='ownersID',
+                         type=owner, location='form', 
+                         help='Car\'s owner')
+
 
 class Car(Resource):
     def get(self, car_id):
@@ -19,10 +44,15 @@ class Car(Resource):
             return '404 Not Found', 404
         return "<p>Deleted car: {}</p>".format(car_id), 204
 
-    def post(self):
-        # add a car
-        pass
-
-    def put(self):
+    def put(self, car_id):
         #update a car
-        pass
+        args = put_parser.parse_args()
+        try:
+            car = Cars.get(car_id)
+        except sq.main.SQLObjectNotFound:
+            return '404 Not Found', 404
+        for k,v in args.items():
+            if v is not None:
+                setattr(car, k, v)
+
+        return car.sqlmeta.asDict(), 201
--- resources/owner.py
+++ resources/owner.py
@@ -1,7 +1,26 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 import sqlobject as sq
 
 from models.model import *
+
+
+def pesel(pesel):
+    # validate pesel
+    if len(pesel) == 14:
+        return pesel
+    else:
+        raise ValueError({'should be of length 14'}.format(pesel))
+
+put_parser = reqparse.RequestParser()
+put_parser.add_argument('first_name', dest='first_name',
+                         location='form',
+                         help='Owner\'s fitsr name')
+put_parser.add_argument('last_name', dest='last_name',
+                         location='form',
+                         help='Owner\'s last name')
+put_parser.add_argument('pesel', dest='pesel',
+                         type=pesel, location='form', 
+                         help='Owner\'s pesel (length 14)')
 
 
 class Owner(Resource):
@@ -20,6 +39,16 @@ class Owner(Resource):
             return '404 Not Found', 404
         return "<p>Deleted owner: {}</p>".format(owner_id), 204
 
-    def put(self):
+    def put(self, owner_id):
         #update a owner
-        pass
+        args = put_parser.parse_args()
+        try:
+            owner = Owners.get(owner_id)
+        except sq.main.SQLObjectNotFound:
+            return '404 Not Found', 404
+        for k,v in args.items():
+            if v is not None:
+                setattr(owner, k, v)
+
+        return owner.sqlmeta.asDict(), 201
+        
