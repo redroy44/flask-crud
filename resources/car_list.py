9e5770ebc04144f720bb3a79314c5bd686a67492
Bandurski, Piotr <piotr.bandurski@intel.com>
2017-06-20 12:02:04 +0200

    post done

--- resources/car_list.py
+++ resources/car_list.py
@@ -1,7 +1,30 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 import sqlobject as sq
 
 from models.model import *
+
+def owner(owner_id):
+    # validate owner_id
+    try:
+        Owners.get(owner_id)
+    except sq.main.SQLObjectNotFound:
+        raise ValueError({"Owner id:{} doesn't exist"}.format(owner_id))
+    return owner_id
+
+
+post_parser = reqparse.RequestParser()
+post_parser.add_argument('brand', dest='brand',
+                         location='form', required=True,
+                         help='Car\'s brand')
+post_parser.add_argument('model', dest='model',
+                         location='form', required=True,
+                         help='Car\'s model')
+post_parser.add_argument('number', dest='number',
+                         location='form', required=True,
+                         help='Car\'s number')
+post_parser.add_argument('owner', dest='owner',
+                         type=owner, location='form', 
+                         required=True, help='Car\'s owner')
 
 class CarList(Resource):
     def get(self):
@@ -14,4 +37,8 @@ class CarList(Resource):
 
     def post(self):
         #add car
-        pass
+        args = post_parser.parse_args()
+        o = Owners.get(args['owner'])
+        Cars(brand=args['brand'],model=args['model'],number=args['number'],ownersID=o)
+
+        return args, 201
--- resources/owner_list.py
+++ resources/owner_list.py
@@ -1,7 +1,25 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 import sqlobject as sq
 
 from models.model import *
+
+def pesel(pesel):
+    # validate pesel
+    if len(pesel) == 14:
+        return pesel
+    else:
+        raise ValueError({'should be of length 14'}.format(pesel))
+
+post_parser = reqparse.RequestParser()
+post_parser.add_argument('first_name', dest='first_name',
+                         location='form', required=True,
+                         help='Owner\'s fitsr name')
+post_parser.add_argument('last_name', dest='last_name',
+                         location='form', required=True,
+                         help='Owner\'s last name')
+post_parser.add_argument('pesel', dest='pesel',
+                         type=pesel, location='form', 
+                         required=True, help='Owner\'s pesel (length 14)')
 
 
 class OwnerList(Resource):
@@ -15,4 +33,6 @@ class OwnerList(Resource):
 
     def post(self):
         #add owner
-        pass
+        args = post_parser.parse_args()
+        Owners(first_name=args['first_name'],last_name=args['last_name'],pesel=args['pesel'])
+        return args, 201
